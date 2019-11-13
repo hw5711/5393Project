590b34ebfdc0550047813cec291f7b74a2998dca
leon541 <leonwang541@gmail.com>
2019-11-13 15:34:03 -0600

    change

--- screenshot.js
+++ screenshot.js
@@ -1,9 +1,13 @@
 // taking screen shot
 const puppeteer = require('puppeteer');
 (async () => {
-    const browser = await puppeteer.launch( {headless:false }); 
+    const browser = await puppeteer.launch( {headless:false}); 
     //const browser = await puppeteer.launch();  // do without UI
     const page = await browser.newPage();
+    await page.setViewport({
+        width: 1200,    
+        height: 800    
+    });
     await page.goto('https://www.google.com');
     await page.screenshot({path: 'google.png'});
     browser.close();
