4ea76e036efa947a67429c7466f1b7ba06e5461a
hw5711 <huanwu5711@gmail.com>
2019-11-16 20:12:49 -0600

    update screenshot

--- testStudentWeb.js
+++ testStudentWeb.js
@@ -8,17 +8,18 @@ const puppeteer = require('puppeteer');
 
     await page.waitFor(1000);
     await page.click('a[name="login"]');
-
+    await page.screenshot({ path: 'login.png' });
     await page.waitFor(1000);
     await page.type('input[name=email]', 'test1@gmail.com');
     await page.type('input[name=password]', '1');
-    await page.screenshot({ path: 'login.png' });
+    await page.screenshot({ path: 'mainPage.png' });
     await page.waitFor(1000);
     await page.click('button[name="login"]');
     await page.waitFor(1000);
     await page.click('a[name="election"]');
     await page.waitFor(2000);
     await page.click('button[name="show"]');
+    await page.screenshot({ path: 'beforeVote.png' });
     await page.waitFor(2000);
     const result = await page.evaluateHandle(() => {
         return document.querySelector(
@@ -31,6 +32,6 @@ const puppeteer = require('puppeteer');
     await page.waitFor(2000);
     await page.click('button[name="show"]');
     await page.waitFor(2000);
-    await page.screenshot({ path: 'election.png' });
+    await page.screenshot({ path: 'afterVote.png' });
     await browser.close();
 })();
